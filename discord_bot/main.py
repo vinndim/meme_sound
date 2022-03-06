7cfb86c2e213b09a058a1a009b0e0a242858c125
vinndim <vinnikov.dima2005@gmail.com>
2022-03-06 20:06:23 +0500

    requirements

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -9,13 +9,10 @@ bot = commands.Bot(command_prefix='!')
 async def join_voice(ctx):
     connected = ctx.author.voice
     if connected:
-        await connected.channel.connect()  # Use the channel instance you put into a variable
+        await connected.channel.connect()
     else:
         await ctx.send("Зайди в голосвой чат, утырок")
 
 
 if __name__ == '__main__':
     bot.run(TOKEN)
-
-# I'm beginning to think they have very similar qualities and can do the same things but is a personal preference to go with a client vs. a bot.
-# However they do have their differences where clients have an on_message while bots wait for a prefix command.
