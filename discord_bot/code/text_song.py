3b93f31a0b53fbf657a50b342d032c5ca11db774
vinndim <vinnikov.dima2005@gmail.com>
2022-03-30 03:34:51 +0500

    show "now" after start song playing

--- discord_bot/code/text_song.py
+++ discord_bot/code/text_song.py
@@ -1,35 +1,44 @@
 import re
+
 import requests
 from bs4 import BeautifulSoup
 
 site_with_lyric = "genius"
 
 
-async def get_lyric(song):
-    search_song = re.sub(r"[$#%!@*&—]", "", song)
+async def get_normal_title(song_title):
+    search_song = re.sub(r"[$#%!@*&—]", "", song_title)
     search_song = re.sub(r'\s+', ' ', search_song)
     try:
         search_song = search_song.split("(")[0] + search_song.split(")")[1]
     except IndexError:
         pass
+    return search_song
+
+
+async def get_lyric(song):
+    search_song = await get_normal_title(song)
     r = requests.get('https://www.google.com/search?q=',
                      params={'q': f'{search_song} {site_with_lyric}'})
     soup = BeautifulSoup(r.content, 'html.parser')
     url = ""
     text = ''
+    msg_text = []
     for link in soup.find_all('a'):
         if "https://genius.com/" in link.get('href'):
             url = link.get('href').split("&")[0][7:]
             break
     if url:
         try:
-            soup = BeautifulSoup(requests.get(url).content, 'lxml')
+            r = requests.get(url)
+            soup = BeautifulSoup(r.content, 'lxml')
             for tag in soup.select('div[class^="Lyrics__Container"], .song_body-lyrics p'):
                 text += tag.get_text(strip=True, separator='\n')
-            if len(text) <= 2000:
-                return [text]
-            else:
-                return [text[:2000], text[2000:]]
+            while len(text) > 2000:
+                msg_text.append(text[:2000])
+                text = text[2000:]
+            msg_text.append(text)
+            return msg_text
         except Exception as e:
             print(e)
     return "**Не найдено на https://genius.com/**"
