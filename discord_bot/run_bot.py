8eb1840e21441fcaedbca5ae4f84fc5e283e9d02
vinndim <vinnikov.dima2005@gmail.com>
2022-04-14 20:21:38 +0500

    bot exits if left alone in voice chat

--- discord_bot/run_bot.py
+++ discord_bot/run_bot.py
@@ -1,9 +1,7 @@
 from discord.ext import commands
 
-from config import TOKEN
+from discord_bot.support_code.config import TOKEN
 from discord_components import DiscordComponents
-
-from music import Music, stop
 
 bot = commands.Bot(command_prefix="!")
 DiscordComponents(bot)
@@ -15,21 +13,22 @@ async def on_ready():
     print('------')
     print("Bot start working...")
     bot.remove_command("help")
-    bot.load_extension("music")
+    cogs = ["discord_bot.cogs.music_cog", "discord_bot.cogs.support_cog"]
+    for cog in cogs:
+        bot.load_extension(cog)
 
 
 @bot.event
 async def on_voice_state_update(member, before, after):
-    try:
-        if len(before.channel.members) == 1:
-            print(member)
-            # await stop(member, before.channel)
-    except AttributeError:
-        pass
     if before.channel is None and after.channel is not None:
         print(f'Пользователь {member} зашёл в канал {after.channel}, в канале {len(after.channel.members)}')
     elif before.channel is not None and after.channel is None:
         print(f'Пользователь {member} вышел из канала {before.channel}, в канале {len(before.channel.members)}')
+        # Бот выходит, если остаётся один в голосовом чате
+        if len(before.channel.members) == 1 and before.channel.members[0].id == bot.user.id:
+            guild_id = before.channel.members[0].guild.id
+            ws = bot._connection._get_websocket(guild_id)
+            await ws.voice_state(str(guild_id), None)
 
 
 bot.run(TOKEN)
