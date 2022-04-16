e5244bc55de1a550f82867b1069b0f1dd5324835
vinndim <vinnikov.dima2005@gmail.com>
2022-04-16 18:56:59 +0500

    clear player after disconnect

--- discord_bot/run_bot.py
+++ discord_bot/run_bot.py
@@ -27,6 +27,8 @@ async def on_voice_state_update(member, before, after):
         # Бот выходит, если остаётся один в голосовом чате
         if len(before.channel.members) == 1 and before.channel.members[0].id == bot.user.id:
             guild_id = before.channel.members[0].guild.id
+            player = bot.music.player_manager.get(guild_id)
+            player.queue.clear()
             ws = bot._connection._get_websocket(guild_id)
             await ws.voice_state(str(guild_id), None)
 
