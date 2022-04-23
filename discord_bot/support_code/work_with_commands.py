c1a0db5c85afed92f55cb10b269817dddb663034
vinndim <vinnikov.dima2005@gmail.com>
2022-04-23 13:58:46 +0500

    add new commands

--- discord_bot/support_code/work_with_commands.py
+++ discord_bot/support_code/work_with_commands.py
@@ -14,6 +14,8 @@ async def get_list_commands():
     em.add_field(name="!play", value="Добавляет песню в очередь")
     em.add_field(name="!stop", value="Отключает бота")
     em.add_field(name="!menu", value="Открывает меню управления песнями")
+    em.add_field(name="!pl", value="Добавляет плейлист с нашего сайта")
+    em.add_field(name="!yandex", value="Добавляет плейлист с YandexMusic")
     em.add_field(name="!now", value="Выводит играющий трек")
     em.add_field(name="!pause", value="Ставит музыку на паузу")
     em.add_field(name="!pl", value="Добавляет плейлист c cайта по названию в очередь")
@@ -23,5 +25,7 @@ async def get_list_commands():
     em.add_field(name="!skip", value="Пропускает текущую песню")
     em.add_field(name="!seek", value="Перематывает на ")
     em.add_field(name="!text", value="Выводит текст текущей песни")
+    em.add_field(name="!shuffle", value="Перемешивает воспроизведение треков")
+    em.add_field(name="!info", value="Информация о пользователе")
     em.add_field(name="!help", value="Выводит список всех команд бота")
     return em
